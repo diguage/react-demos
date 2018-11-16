6f2d57aba243f216d7c42b138afb87b00596c36e
diguage <leejun119@gmail.com>
2018-11-17 00:47:54 +0800

    finish redux demo

--- demo02-redux/src/index.js
+++ demo02-redux/src/index.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import myApp from "./reducers";
+import App from "./App";
+import Results from "./components/results";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "bootstrap/dist/css/bootstrap.min.css";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+let store = createStore(myApp);
+
+function render() {
+  ReactDOM.render(
+    <div className="container">
+      <App store={store} />
+      <hr />
+      <Results store={store} />
+    </div>,
+    document.getElementById("root")
+  );
+}
+
+store.subscribe(render);
+
+render();
