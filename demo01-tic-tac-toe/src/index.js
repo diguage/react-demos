65c263a832c5cf072d5944831270a53cfb3b5b47
diguage <leejun119@gmail.com>
2018-11-16 12:20:43 +0800

    Test lifecycle

--- demo01-tic-tac-toe/src/index.js
+++ demo01-tic-tac-toe/src/index.js
@@ -66,6 +66,29 @@ class Game extends React.Component {
     };
   }
 
+  componentWillMount() {
+    console.log('Component WILL MOUNT!');
+  }
+  componentDidMount() {
+    console.log('Component DID MOUNT!');
+  }
+  componentWillReceiveProps(newProps) {
+    console.log('Component WILL RECIEVE PROPS!');
+  }
+  shouldComponentUpdate(newProps, newState) {
+    console.log('Will update component!');
+    return true;
+  }
+  componentWillUpdate(nextProps, nextState) {
+    console.log('Component WILL UPDATE!');
+  }
+  componentDidUpdate(prevProps, prevState) {
+    console.log('Component DID UPDATE!');
+  }
+  componentWillUnmount() {
+    console.log('Component WILL UNMOUNT!');
+  }
+
   handleClick(i) {
     const history = this.state.history;
     const current = history[history.length - 1];
