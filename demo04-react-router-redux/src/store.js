c706b2a868ea87884a9dba5250d2c1ade734fa2b
diguage <leejun119@gmail.com>
2018-11-24 10:21:56 +0800

    fix bug

--- demo04-react-router-redux/src/store.js
+++ demo04-react-router-redux/src/store.js
@@ -13,7 +13,7 @@ const middleware = [thunk, routerMiddleware(history)];
 if (process.env.NODE_ENV === "development") {
   const devToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__;
   if (typeof devToolsExtension === "function") {
-    enhancers.push(devToolsExtension);
+    enhancers.push(devToolsExtension());
   }
 }
 
